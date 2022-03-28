6d4410dcc2c73331acad0216fc8ff41ecb33cef8
zhangyu ye <darrenyipzy@gmail.com>
2022-03-28 23:59:40 +0800

    invoices mobile

--- src/components/invoice.jsx
+++ src/components/invoice.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import clsx from "clsx";
 
 const Invoice = (props) => {
   const { _id, buyer, price, due, status } = props.data;
+  const statusColorClass = clsx({
+    "invoices--invoice__status": true,
+    "d-flex-end": true,
+    "clr-draft": status == 0,
+    "clr-pending": status == 1,
+    "clr-paid": status == 2,
+  });
+  console.log(statusColorClass);
   return (
     <div className="invoices--invoice">
       <div className="invoices--invoice__id ">
@@ -14,9 +23,14 @@ const Invoice = (props) => {
       <div className="invoices--invoice__due">
         <p>{due}</p>
       </div>
-      <div className="invoices--invoice__price">{price}</div>
-      <div className="invoices--invoice__status d-flex-center">
-        status: {status}
+      <div className="invoices--invoice__price">
+        <p className="f-bold">$ {price}</p>
+      </div>
+      <div className={statusColorClass}>
+        <div className="invoices--invoice__status--wpr d-flex-center">
+          <div className="dot"></div>
+          <p>{status == 0 ? "Draft" : status == 1 ? "Pending" : "Paid"}</p>
+        </div>
       </div>
     </div>
   );
