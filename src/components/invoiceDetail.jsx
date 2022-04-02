33c4badf5744a45f1155be1ee5fc951d90782c50
zhangyu ye <darrenyipzy@gmail.com>
2022-04-02 12:34:15 +0800

    update invoice detail page

--- src/components/invoiceDetail.jsx
+++ src/components/invoiceDetail.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useStore from "../services/store";
+import clsx from "clsx";
 const InvoiceDetail = (props) => {
   let navigate = useNavigate();
-  const statusColorClass = {};
   const { id } = useParams();
   const invoices = useStore((state) => state.invoices);
   const invoice = invoices.find((item) => item._id === id);
+  const statusColorClass = clsx({
+    "clr-draft": invoice.status == "Draft",
+    "clr-pending": invoice.status == "Pending",
+    "clr-paid": invoice.status == "Paid",
+  });
 
   return (
     <React.Fragment>
@@ -31,43 +36,45 @@ const InvoiceDetail = (props) => {
         </div>
         <div className="invoice-detail--sheet card">
           <div className="invoice-detail--sheet__order">
-            <p className="f-bold">#{invoice._id}</p>
+            <p className="f-bold invoice-id">
+              # <p className="clr-black ">{invoice._id}</p>
+            </p>
             <p className="clr-purple">Graphic design</p>
           </div>
           <div className="invoice-detail--sheet__address">
-            <p>19 df</p>
-            <p>lodon</p>
+            <p>18 Union Terrace</p>
+            <p>Londong</p>
             <p>EI 3EA</p>
             <p>United Kingdom</p>
           </div>
           <div className="invoice-detail--sheet__date-bill">
             <div className="invoice-detail--sheet__date-bill--date">
-              <p className="clr-purple">Invoice Date</p>
+              <p className="clr-purple title-sm">Invoice Date</p>
               <h3>21 Aug 2022</h3>
             </div>
             <div className="invoice-detail--sheet__date-bill--due">
-              <p className="clr-purple">Payment Due</p>
+              <p className="clr-purple title-sm">Payment Due</p>
               <h3>20 Sep 2022</h3>
             </div>
             <div className="invoice-detail--sheet__date-bill--bill">
-              <p className="clr-purple">Bill To</p>
-              <h3>Alex Grim</h3>
-              <p>19 df</p>
-              <p>lodon</p>
-              <p>EI 3EA</p>
-              <p>United Kingdom</p>
+              <p className="clr-purple title-sm">Bill To</p>
+              <h3 className="name">Alex Grim</h3>
+              <p className="address-line">19 Church Way</p>
+              <p className="address-line">London</p>
+              <p className="address-line">EI 3EA</p>
+              <p className="address-line">United Kingdom</p>
             </div>
           </div>
           <div className="invoice-detail--sheet__sent-to">
             <p>Sent to</p>
             <h3>alexgrim@mail.com</h3>
           </div>
-          <div className="invoice-detail--sheet--products-wpr">
+          <div className="invoice-detail--sheet__products-wpr only-mobile">
             <div className="products-top">
               <div className="products-top--item">
                 <div className="products-top--item__detail">
-                  <p className="f-bold">Banner Design</p>
-                  <p className="clr-purple">1 x $99.9</p>
+                  <p className="f-bold detail-name">Banner Design</p>
+                  <p className="clr-purple detail-price">1 x $99.9</p>
                 </div>
                 <div className="products-top--item__amount">
                   <p className="f-bold">$ 99.99</p>
