4fa86494be114eaa105ca2ecd6f4ead558ec9b97
zhangyu ye <darrenyipzy@gmail.com>
2022-04-20 21:15:24 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -9,43 +9,31 @@ import { fakerData } from "./services/fakeData";
 import "./App.scss";
 
 function App() {
-    const theme = useStore((state) => state.theme);
-    const setInvoices = useStore((state) => state.setInvoices);
-    const saveData = fakerData();
-    // console.log(saveData);
-    setInvoices(saveData);
-    const AppClasses = clsx({
-        App: true,
-        "d-flex-lg": true,
-        dark: theme === "dark",
-        light: theme !== "dark",
-    });
-    // console.log(AppClasses);
-    return ( <
-        div className = { AppClasses } >
-        <
-        Navbar / >
-        <
-        main >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Invoices / > }
-        /> <
-        Route path = "invoices"
-        element = { < Invoices / > }
-        /> <
-        Route path = "invoices/:id"
-        element = { < InvoiceDetail / > }
-        /> <
-        Route path = "invoices/new"
-        element = { < NewInvoice / > }
-        /> <
-        /Routes> <
-        /main> <
-        /div>
-    );
+  const theme = useStore((state) => state.theme);
+  const setInvoices = useStore((state) => state.setInvoices);
+  const saveData = fakerData();
+  // console.log(saveData);
+  setInvoices(saveData);
+  const AppClasses = clsx({
+    App: true,
+    "d-flex-lg": true,
+    dark: theme === "dark",
+    light: theme !== "dark",
+  });
+  // console.log(AppClasses);
+  return (
+    <div className={AppClasses}>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Invoices />} />
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="invoices/:id" element={<InvoiceDetail />} />
+          <Route path="invoices/new" element={<NewInvoice />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
